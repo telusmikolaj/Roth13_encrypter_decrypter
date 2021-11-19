e881d34d548c6ce28cf75f4c5661cc2db58d983f
Mikolaj Telus <mikolaj.telus@gmail.com>
2021-11-19 13:00:35 +0100

    add decrypter menu and function for decrypting given text

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Encrypter:
         self.key = 13
         self.encrypted_texts = []
 
-    def encrypt(self, text):
+    def encrypt_or_decrypt(self, text, mode):
         ciphertext = ''
         for c in text:
             if c.isupper():
@@ -12,9 +12,13 @@ class Encrypter:
             else:
                 ciphertext += chr((ord(c) + self.key - 97) % 26 + 97)
 
-        self.show_save_file_menu(ciphertext)
+        if mode == 'encrypt':
+            self.show_save_file_menu(text, ciphertext)
+        elif mode == 'decrypt':
+            print('Decrypted ' + ciphertext)
 
-    def show_save_file_menu(self, encrypted_text):
+
+    def show_save_file_menu(self,text, encrypted_text):
         user_input = ' '
         while user_input != 1 or user_input != 2 or user_input != 3:
             user_input = int(input(f'1. Save encrypted text to list \n'
@@ -27,7 +31,7 @@ class Encrypter:
                 self.save_to_file(encrypted_text)
                 break
             else:
-                print(f'Encrpyted text: {encrypted_text} \n')
+                print(f'{text}: {encrypted_text} \n')
                 break
 
     def save_to_list(self, encrypted_text):
@@ -49,7 +53,6 @@ class Encrypter:
             print('Text is already in file')
         else:
             with open('encrypted_texts.txt', 'a') as f:
-                print ('Hallo')
                 f.write(encrypted_text)
                 f.write('\n')
 
@@ -62,23 +65,43 @@ class Menu:
     def __init__(self):
         self.userInput = ' '
         self.encrypter = Encrypter()
-        self.show_menu()
+        self.show_main_menu()
 
-    def show_menu(self):
+    def show_main_menu(self):
         while self.userInput:
             self.userInput = int(input(
                 f'Menu \n'
-                f'1. Encrpyt text \n'
+                f'1. Encrpyt text and save to file or list \n'
                 f'2. Decrypt text \n'
-                f'3. Show encrypted texts \n'
+                f'5. Show encrypted texts \n'
                 f'4. Exit \n'))
 
             self.start_encrypter(self.userInput)
 
+    def show_decrypt_menu(self):
+        decrypt_option = 0
+        while decrypt_option != 1 or decrypt_option != 2 or decrypt_option != 3:
+            decrypt_option = int(input(
+                f'1. Decrypt given text \n'
+                f'2. Decrypt text form list \n'
+                f'3. Decrpyt text from file \n '))
+
+            self.start_decrypter(decrypt_option)
+
     def start_encrypter(self, user):
         if user == 1:
-            text_to_encrypt = str(input('Podaj slowo do zaszyfrowania: '))
-            self.encrypter.encrypt(text_to_encrypt)
+            text_to_encrypt = str(input('Enter text to be encrypted: \n'))
+            self.encrypter.encrypt_or_decrypt(text_to_encrypt,'encrypt')
+        if user == 2:
+            self.show_decrypt_menu()
+
+    def start_decrypter(self, decrypt_option):
+        if decrypt_option == 1:
+            text_to_decrypt = str(input('Enter text to be decrypted: \n'))
+            self.encrypter.encrypt_or_decrypt(text_to_decrypt,'decrypt')
+
+
+
 
 
 
