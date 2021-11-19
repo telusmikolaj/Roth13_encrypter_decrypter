398736609abb1977c056f4489a1ba9c47d6d8931
Mikolaj Telus <mikolaj.telus@gmail.com>
2021-11-19 21:01:14 +0100

    add function that allows decrypt text from file

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ class Encrypter:
     def show_save_file_menu(self, text, encrypted_text):
         user_input = ' '
         while user_input != 1 or user_input != 2 or user_input != 3:
-            user_input = int(input(f'1. Save encrypted text to list \n'
+            user_input = input(f'1. Save encrypted text to list \n'
                                    f'2. Save encrypted text to file \n'
-                                   f'3. Only show encrypted text \n '))
+                                   f'3. Only show encrypted text \n ')
             if user_input == 1:
                 self.save_to_list(encrypted_text)
                 break
             elif user_input == 2:
                 self.save_to_file(encrypted_text)
                 break
-            else:
+            elif user_input == 3:
                 print(f'{text}: {encrypted_text} \n')
                 break
 
@@ -66,12 +66,13 @@ class Encrypter:
             print('File is empty \n')
         else:
             print('Encrypted Texts: ')
-            for line in self.encrypted_texts_form_file:
-                print(line)
+            for index, line in enumerate(self.encrypted_texts_form_file, start=1):
+                print(f'{index}. {line}')
 
         print('\n')
 
     def get_encrypted_texts_from_file(self):
+        self.encrypted_texts_form_file.clear()
         with open('encrypted_texts.txt') as f:
             for index, line in enumerate(f):
                 self.encrypted_texts_form_file.append(line.strip())
@@ -81,16 +82,32 @@ class Encrypter:
             if encrypted_text in f.read():
                 return True
 
-    def decrypt_text_from_list(self, text_id):
-        for index, line in enumerate(self.encrypted_texts, start=1):
-            if index == text_id:
-                self.encrypt_or_decrypt(line,'decrypt')
-                valid_id = True
-            else:
+    def decrypt_text_from_list_or_file(self, text_id, mode):
+        is_not_valid_id = True
+        if mode == 'list':
+            for index, line in enumerate(self.encrypted_texts, start=1):
+                if index == text_id:
+                    self.encrypt_or_decrypt(line, 'decrypt')
+                    is_not_valid_id = False
+            if is_not_valid_id:
+                print('Invalid id')
+        elif mode == 'file':
+            self.get_encrypted_texts_from_file()
+            for index, line in enumerate(self.encrypted_texts_form_file, start=1):
+                if index == text_id:
+                    self.encrypt_or_decrypt(line, 'decrypt')
+                    is_not_valid_id = False
+
+            if is_not_valid_id:
                 print('Invalid id')
 
     def get_number_of_text_in_list(self):
         return len(self.encrypted_texts)
+
+    def get_number_of_texts_in_file(self):
+        self.get_encrypted_texts_from_file()
+        return len(self.encrypted_texts_form_file)
+
 
 class Menu:
     def __init__(self):
@@ -149,10 +166,16 @@ class Menu:
             else:
                 self.encrypter.show_list()
                 text_id = int(input('Enter id of text from list to decrypt: \n'))
-                self.encrypter.decrypt_text_from_list(text_id)
+                self.encrypter.decrypt_text_from_list_or_file(text_id, 'list')
+        if decrypt_option == 3:
+            if self.encrypter.encrypted_texts_form_file == 0:
+                print('File is empty')
+            else:
+                self.encrypter.display_encrypted_texts_from_file()
+                text_id = int(input('Enter id of text from file to decrypt: \n'))
+                self.encrypter.decrypt_text_from_list_or_file(text_id, 'file')
 
     def display_decrypted_texts(self, display_option):
-
         if display_option == 1:
             self.encrypter.show_list()
         else:
@@ -161,11 +184,3 @@ class Menu:
 
 menu = Menu()
 
-# list = []
-# with open('encrypted_texts.txt') as f:
-#     for index, line in enumerate(f):
-#         print(line)
-#         list.append(line.strip())
-#
-# for index, line in enumerate(list):
-#     print(index, line)
