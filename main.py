e8b79813f95d6d55f1f98b0f3a127fd16a5d199c
Mikolaj Telus <mikolaj.telus@gmail.com>
2021-11-19 11:22:48 +0100

    add class Menu with user options

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-def encrypt_or_decrypt(text):
-    ciphertext = ''
-    key = 13
-    for c in text:
-        if c.isupper():
-            ciphertext += chr((ord(c) + key - 65) % 26 + 65)
-        else:
-            ciphertext += chr((ord(c) + key - 97) % 26 + 97)
 
-    return ciphertext
+class Encrypter:
+    def __init__(self):
+        self.key = 13
+        self.encrypted_texts = []
+
+    def encrypt(self, text):
+        ciphertext = ''
+        for c in text:
+            if c.isupper():
+                ciphertext += chr((ord(c) + self.key - 65) % 26 + 65)
+            else:
+                ciphertext += chr((ord(c) + self.key - 97) % 26 + 97)
+
+class Menu:
+    def __init__(self):
+        self.userInput = ' '
+        self.encrypter = Encrypter()
+        self.show_menu()
+
+    def show_menu(self):
+        while self.userInput:
+            self.userInput = int(input(
+                f'Menu \n'
+                f'1. Encrpyt text and save to list \n'
+                f'2. Decrypt text \n'
+                f'3. List Decrypted texts'
+                f'4. Exit \n'))
+
+            self.start_encrypter(self.userInput)
+
+    def start_encrypter(self, user):
+        if user == 1:
+            text_to_encrypt = str(input('Podaj slowo do zaszyfrowania: '))
+            self.encrypter.encrypt(text_to_encrypt)
+
+
+
+menu = Menu()
+menu.show_menu()
