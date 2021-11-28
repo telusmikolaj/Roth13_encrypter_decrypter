770088a2534161142517d8cf0f1f8962ba2583a0
Mikolaj Telus <mikolaj.telus@gmail.com>
2021-11-28 15:37:55 +0100

    fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# PSL
+"""PSL"""
 from inspect import cleandoc
 from typing import Dict, NoReturn
 
@@ -7,57 +7,73 @@ from rot_13 import Decrypter, Encrypter
 
 
 class MainMenu:
+    """
+    A class to represent main menu.
+
+    Attributes
+    ----------
+    encrypter : class
+        class of encrpyter
+    decrypter : class
+    class of decrypter
+
+    """
+
     def __init__(self) -> NoReturn:
         self.encrypter = Encrypter()
         self.decrypter = Decrypter()
 
     def invoke_main_loop(self) -> NoReturn:
+        """Invokes main menu loop"""
         avalible_choices: Dict[str, str] = {
             "1": self.invoke_encrypter_menu_loop,
             "2": self.invoke_decrypter_menu_loop,
             "3": self.invoke_display_menu_loop,
-            "4": self.invoke_delete_menu_loop}
+            "4": self.invoke_delete_menu_loop,
+        }
         while 1:
             print(self.show_main_menu())
             user_input_ = input("> ")
             if user_input_ in avalible_choices:
                 avalible_choices[user_input_]()
-            elif user_input_ == '5':
+            elif user_input_ == "5":
                 break
             else:
                 print("Wrong choice!")
 
-    def show_main_menu(self) -> str:
+    @classmethod
+    def show_main_menu(cls) -> str:
+        """Return main menu"""
         return cleandoc(
             """
-            Menu
-            1. Encrypt text 
-            2. Decrypt text 
-            3. Display encrypted texts
-            4. Delete encrypted texts form list or file
-            5. Exit
-            """
+                1. Encrypt text and save to list or file
+                2. Decrypt text from list or file
+                3. Display form list or file
+                4. Delete text form list or file
+                5. Exit
+             """
         )
 
     def invoke_encrypter_menu_loop(self) -> NoReturn:
+        """Invokes encrypter menu loop"""
         avalible_encrypter_options: Dict[str, str] = {
-            '1': self.encrypter.encrypt_without_saving,
-            '2': self.encrypter.save_to_list,
-            '3': self.encrypter.save_to_file,
-
+            "1": self.encrypter.encrypt_without_saving,
+            "2": self.encrypter.save_to_list,
+            "3": self.encrypter.save_to_file,
         }
         while 1:
             print(self.show_encrypter_menu())
             user_input_ = input("> ")
             if user_input_ in avalible_encrypter_options:
                 avalible_encrypter_options[user_input_]()
-            elif user_input_ == '4':
+            elif user_input_ == "4":
                 break
             else:
                 print("Wrong choice!")
 
-    def show_encrypter_menu(self) -> str:
-
+    @classmethod
+    def show_encrypter_menu(cls) -> str:
+        """Return encrpyter menu"""
         return cleandoc(
             """
                 1. Only print encrypted text
@@ -68,23 +84,25 @@ class MainMenu:
         )
 
     def invoke_decrypter_menu_loop(self) -> NoReturn:
+        """Invokes decrypter menu loop"""
         avalible_decrypter_options: Dict[str, str] = {
-            '1': self.decrypter.decrypt_text_from_user,
-            '2': self.decrypter.decrypt_text_from_list,
-            '3': self.decrypter.decrypt_text_from_file,
+            "1": self.decrypter.decrypt_text_from_user,
+            "2": self.decrypter.decrypt_text_from_list,
+            "3": self.decrypter.decrypt_text_from_file,
         }
         while 1:
             print(self.show_decrypt_menu())
             user_input_ = input("> ")
             if user_input_ in avalible_decrypter_options:
                 avalible_decrypter_options[user_input_]()
-            elif user_input_ == '4':
+            elif user_input_ == "4":
                 break
             else:
                 print("Wrong choice!")
 
-    def show_decrypt_menu(self) -> str:
-
+    @classmethod
+    def show_decrypt_menu(cls) -> str:
+        """Return decrypt menu"""
         return cleandoc(
             """
                 1. Decrypt given text
@@ -94,24 +112,25 @@ class MainMenu:
                     """
         )
 
-
     def invoke_display_menu_loop(self) -> NoReturn:
+        """Invokes display menu loop"""
         avalible_display_options: Dict[str, str] = {
-            '1': self.encrypter.show_encrypted_texts_from_list,
-            '2': self.encrypter.show_encrypted_texts_from_file
+            "1": self.encrypter.show_encrypted_texts_from_list,
+            "2": self.encrypter.show_encrypted_texts_from_file,
         }
         while 1:
             print(self.show_display_menu())
             user_input_ = input("> ")
             if user_input_ in avalible_display_options:
                 avalible_display_options[user_input_]()
-            elif user_input_ == '3':
+            elif user_input_ == "3":
                 break
             else:
                 print("Wrong choice!")
 
-    def show_display_menu(self) -> str:
-
+    @classmethod
+    def show_display_menu(cls) -> str:
+        """Return display menu"""
         return cleandoc(
             """
                 1. Display texts in list
@@ -121,22 +140,24 @@ class MainMenu:
         )
 
     def invoke_delete_menu_loop(self) -> NoReturn:
+        """Invokes delete menu loop"""
         avalible_delete_options: Dict[str, str] = {
-            '1': self.encrypter.delete_text_from_list,
-            '2': self.encrypter.delete_text_from_file
+            "1": self.encrypter.delete_text_from_list,
+            "2": self.encrypter.delete_text_from_file,
         }
         while 1:
             print(self.show_delete_menu())
             user_input_ = input("> ")
             if user_input_ in avalible_delete_options:
                 avalible_delete_options[user_input_]()
-            elif user_input_ == '3':
+            elif user_input_ == "3":
                 break
             else:
                 print("Wrong choice!")
 
-    def show_delete_menu(self) -> str:
-
+    @classmethod
+    def show_delete_menu(cls) -> str:
+        """Return delete menu"""
         return cleandoc(
             """
                 1. Delete text from list
@@ -144,6 +165,7 @@ class MainMenu:
                 3. Return
             """
         )
+
 
 if __name__ == "__main__":
     MainMenu().invoke_main_loop()
--- rot_13.py
+++ rot_13.py
@@ -1,6 +1,5 @@
-# PSL
-
-from typing import Dict, Union, NoReturn
+"""PSL"""
+from typing import NoReturn
 
 # Own
 from untils import get_text_from_user
@@ -8,26 +7,57 @@ from untils import get_text_id_from_user
 
 
 class Encrypter:
+    """
+    A class to represent a encrypter that encrypts with rot13.
+
+    Attributes
+    ----------
+    key : str
+        ciper key of encrypter
+    encrypted_texts : list
+        encrpyted texts
+    encrpyted_texts_from_file : list
+        encrypted texts form file
+
+    """
+
     def __init__(self):
         self.key = 13
         self.encrypted_texts = []
         self.encrypted_texts_form_file = []
 
-    def encrypt_or_decrypt(self, text_to_encrypt_or_decrypt: str = '', encrypted_or_decrypted_text: str = '', ) -> str:
-        for c in text_to_encrypt_or_decrypt:
-            if c.isupper():
-                encrypted_or_decrypted_text += chr((ord(c) + self.key - 65) % 26 + 65)
+    def encrypt_or_decrypt(
+            self,
+            text_to_encrypt_or_decrypt: str = "",
+            encrypted_or_decrypted_text: str = "",
+    ) -> str:
+        """
+        Return encrypted/decrypted text
+        """
+        for char in text_to_encrypt_or_decrypt:
+            if char.isupper():
+                encrypted_or_decrypted_text += chr(
+                    (ord(char) + self.key - 65) % 26 + 65
+                )
             else:
-                encrypted_or_decrypted_text += chr((ord(c) + self.key - 97) % 26 + 97)
+                encrypted_or_decrypted_text += chr(
+                    (ord(char) + self.key - 97) % 26 + 97
+                )
 
         return encrypted_or_decrypted_text
 
     def encrypt_without_saving(self) -> NoReturn:
-        text_to_encrypt = get_text_from_user('Enter text to encrypt')
+        """
+        Only print encrypted text
+        """
+        text_to_encrypt = get_text_from_user("Enter text to encrypt")
         encrypted_text = self.encrypt_or_decrypt(text_to_encrypt)
-        print(f'Encrypted text - {encrypted_text}')
+        print(f"Encrypted text - {encrypted_text}")
 
     def save_to_list(self) -> NoReturn:
+        """
+        Save enrypted text to list
+        """
         text_to_encrypt = get_text_from_user("Enter text and save to list: ")
         encrypted_text = self.encrypt_or_decrypt(text_to_encrypt)
         if encrypted_text in self.encrypted_texts:
@@ -37,58 +67,80 @@ class Encrypter:
         self.show_encrypted_texts_from_list()
 
     def show_encrypted_texts_from_list(self) -> NoReturn:
+        """
+        Display encrypted text saved on list
+        """
         if not self.is_list_empty():
             print("Encrypted texts list: ")
             for index, line in enumerate(self.encrypted_texts, start=1):
                 print(f"{index}. {line}")
 
-        print(f'List is empty \n')
+        print("List is empty \n")
 
     def save_to_file(self) -> NoReturn:
+        """
+        Save encrypted text to file
+        """
         text_to_encrypt = get_text_from_user("Enter text and save to file: ")
         encrypted_text = self.encrypt_or_decrypt(text_to_encrypt)
         if self.is_text_in_file(encrypted_text):
             print("Text is already in file")
         else:
-            with open("encrypted_texts.txt", "a") as f:
-                f.write(encrypted_text)
-                f.write("\n")
+            with open("encrypted_texts.txt", "a", encoding="utf8") as file:
+                file.write(encrypted_text)
+                file.write("\n")
 
-    def is_text_in_file(self, encrypted_text) -> bool:
-        with open("encrypted_texts.txt") as f:
-            if encrypted_text in f.read():
+    @classmethod
+    def is_text_in_file(cls, encrypted_text) -> bool:
+        """
+        Checks if the file contains text
+        """
+        with open("encrypted_texts.txt", encoding="utf8") as file:
+            if encrypted_text in file.read():
                 return True
         return False
 
     def show_encrypted_texts_from_file(self) -> NoReturn:
+        """
+        Display encrypted texts from file
+
+        """
         self.get_encrypted_texts_from_file()
         if not self.is_file_empty():
             print("Encrypted Texts: ")
             for index, line in enumerate(self.encrypted_texts_form_file, start=1):
                 print(f"{index}. {line}")
 
-        print(f'List is empty \n')
+        print("List is empty \n")
 
     def delete_text_from_file(self) -> NoReturn:
+        """
+        Delete text from the file using the given id
+
+        """
         self.get_encrypted_texts_from_file()
         self.show_encrypted_texts_from_file()
-        text_id = get_text_id_from_user('Enter text id: ')
+        text_id = get_text_id_from_user("Enter text id: ")
         if self.encrypted_texts_form_file:
-            with open("encrypted_texts.txt", "w") as fp:
+            with open("encrypted_texts.txt", "w", encoding="utf8") as file:
                 for index, line in enumerate(self.encrypted_texts_form_file, start=1):
                     if index != int(text_id):
-                        fp.write(line)
-                        fp.write("\n")
+                        file.write(line)
+                        file.write("\n")
 
         else:
             print("File is empty")
 
     def delete_text_from_list(self) -> NoReturn:
+        """
+        Delete text from the list using the given id
+
+        """
         if self.encrypted_texts:
             is_not_valid_id = True
-            self.show_list()
-            text_id = get_text_id_from_user('Enter text id: ')
-            for index, line in enumerate(self.encrypted_texts, start=1):
+            self.show_encrypted_texts_from_list()
+            text_id = get_text_id_from_user("Enter text id: ")
+            for index in enumerate(self.encrypted_texts, start=1):
                 if index == int(text_id):
                     del self.encrypted_texts[int(text_id) - 1]
                     is_not_valid_id = False
@@ -98,56 +150,84 @@ class Encrypter:
         else:
             print("List is empty")
 
-
     def get_encrypted_texts_from_file(self) -> NoReturn:
+        """
+        Return encrpyted texts from file
+
+        """
         self.encrypted_texts_form_file.clear()
-        with open("encrypted_texts.txt") as f:
-            for index, line in enumerate(f):
+        with open("encrypted_texts.txt", encoding="utf8") as file:
+            for line in enumerate(file):
                 self.encrypted_texts_form_file.append(line.strip())
 
     def is_list_empty(self) -> bool:
+        """
+        Checks if the list is empty
+
+        """
         return len(self.encrypted_texts) <= 0
 
     def is_file_empty(self) -> bool:
+        """
+        Checks if the file is empty
+
+        """
         self.get_encrypted_texts_from_file()
         return len(self.encrypted_texts_form_file) <= 0
 
 
 class Decrypter(Encrypter):
+    """
+        A class to represent a decrypter that decrypts text encrypted with rot13.
+
+    """
 
     def decrypt_text_from_user(self) -> NoReturn:
-        text_to_decrypt = get_text_from_user('Enter text to decrypt: ')
+        """
+        Decrpyt text given from user
+
+        """
+        text_to_decrypt = get_text_from_user("Enter text to decrypt: ")
         decrypted_text = self.encrypt_or_decrypt(text_to_decrypt)
-        return print(f'Decrypted - {decrypted_text} ')
+        return print(f"Decrypted - {decrypted_text} ")
 
-    def decrypt_text_from_list(self) -> str:
+    def decrypt_text_from_list(self) -> NoReturn:
+        """
+        Decrpyt text from the list using id given form user
 
+        """
         if not self.is_list_empty():
             self.show_encrypted_texts_from_list()
-            text_id = get_text_id_from_user('Enter text id: ')
+            text_id = get_text_id_from_user("Enter text id: ")
             is_not_valid_id = True
             for index, encrypted_text in enumerate(self.encrypted_texts, start=1):
                 if index == text_id:
                     decrypted_text = self.decrypt_text(encrypted_text)
-                    return print(f'{encrypted_text} - {decrypted_text} ')
+                    print(f"{encrypted_text} - {decrypted_text} ")
                     is_not_valid_id = False
             if is_not_valid_id:
-                print('Invalid id')
+                print("Invalid id")
 
-        return print('List is empty. Nothing to decrypt!')
+        print("List is empty. Nothing to decrypt!")
 
-    def decrypt_text_from_file(self) -> str:
+    def decrypt_text_from_file(self) -> NoReturn:
+        """
+        Decrpyt text from the file using id given form user
+
+        """
         if not self.is_file_empty():
             self.show_encrypted_texts_from_file()
-            text_id = get_text_id_from_user('Enter text id: ')
+            text_id = get_text_id_from_user("Enter text id: ")
             self.get_encrypted_texts_from_file()
             is_not_valid_id = True
-            for index, encrypted_text in enumerate(self.encrypted_texts_form_file, start=1):
+            for index, encrypted_text in enumerate(
+                    self.encrypted_texts_form_file, start=1
+            ):
                 if index == text_id:
                     decrypted_text = self.encrypt_or_decrypt(encrypted_text)
-                    return print(f'{encrypted_text} - {decrypted_text} ')
                     is_not_valid_id = False
+                    print(f"{encrypted_text} - {decrypted_text} ")
             if is_not_valid_id:
-                return print("Invalid id")
+                print("Invalid id")
 
-        return print('File is empty. Nothing to decrypt')
+        print("File is empty. Nothing to decrypt")
